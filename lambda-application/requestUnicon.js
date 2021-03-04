a1554d0a452cef39b8334cabb046a46025810bb2
Yasufumi Miyazaki <yasufumimiyazaki@yasufuminoMacBook-Air.local>
2021-03-04 13:34:44 +0900

    ch requestUnicon.js

--- lambda-application/requestUnicon.js
+++ lambda-application/requestUnicon.js
@@ -87,7 +87,7 @@ function findUnicorn(pickupLocation) {
 
 function recordRide(rideId, username, unicorn) {
     return ddb.put({
-        TableName: 'Rides',
+        TableName: '', # .ex table-name: handson-dynamodb-username
         Item: {
             RideId: rideId,
             User: username,
